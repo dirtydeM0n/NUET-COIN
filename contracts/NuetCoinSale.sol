db978c6e2fb09eb59c3e49321cae960280aceb1b
dirtydeM0n <31212069+dirtydeM0n@users.noreply.github.com>
2019-02-24 02:57:53 +0500

    Coin Sale Contract
    Added buying tokens functionality

--- contracts/NuetCoinSale.sol
+++ contracts/NuetCoinSale.sol
@@ -5,6 +5,8 @@ contract NuetCoinSale {
     address admin;
     NuetCoin public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(NuetCoin _tokenContract, uint256 _tokenPrice) public {
         // assign an admin which will have control over this tokensale contract
@@ -13,5 +15,26 @@ contract NuetCoinSale {
         tokenContract = _tokenContract;
         // Token Price in exchange for ethers
         tokenPrice = _tokenPrice;
+    }
+    // from the DS math library
+    // internal function cannot be accessed externally
+    // pure function do not interact with the blockchain and is purely a normal function
+    function multiply(uint256 x, uint256 y) internal pure returns(uint256 z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        // require that the value is equal to the tokens in ether and prevention from under or overpaying
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        // require that the contract have enough contracts
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        // Check whether a transfer was successfull or not
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        // keep track of the tokens sold
+        tokensSold += _numberOfTokens;
+        // emiting a sell event
+        emit Sell(msg.sender, _numberOfTokens); // the person calling this function will be the buyer
+
+
     }
 }
--- test/NuetCoinSale.js
+++ test/NuetCoinSale.js
@@ -1,8 +1,14 @@
+var NuetCoin = artifacts.require("./NuetCoin.sol");
 var NuetCoinSale = artifacts.require("./NuetCoinSale.sol");
 
 contract('NuetCoinSale', function(accounts){
+    var tokenInstance;
     var tokenSaleInstance;
     var tokenPrice = 1000000000000000; // the price is wei 0.001 ether
+    var buyer = accounts[1];
+    var admin = accounts[0];
+    var tokensAvailable = 750000; // 75percent from total supply available
+    var numberOfTokens;
     it('Initializes the contract with the correct values', function(){
         return NuetCoinSale.deployed().then(function(instance) {
             tokenSaleInstance = instance;
@@ -15,6 +21,46 @@ contract('NuetCoinSale', function(accounts){
             return tokenSaleInstance.tokenPrice();
         }).then(function(price){
             assert.equal(price, tokenPrice, 'token price is correct');
+        });
+    });
+    it('facilitates token buying', function(){
+        return NuetCoin.deployed().then(function(instance){
+            // grabbing token instance first
+            tokenInstance = instance;
+
+            return NuetCoinSale.deployed();
+        }).then(function(instance) {
+            // then grab token sale instance
+            tokenSaleInstance = instance;
+            // Provisioning 75% of tokens to the coin-sale contract from nuetcoin contract
+            return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, {from: admin});
+        }).then(function(receipt) {
+            numberOfTokens = 10;
+            
+            return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value:  tokenPrice * numberOfTokens});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Sell', 'triggers one event');
+			assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+			assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            
+            return tokenSaleInstance.tokensSold();
+        }).then(function(amount){
+            assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+            return tokenInstance.balanceOf(buyer);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), numberOfTokens);
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+            // trying to buy tokens different from the ether value
+            // prevention from under or overpaying
+            return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: 1});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+            return tokenSaleInstance.buyTokens(800000, {from: buyer, value:  tokenPrice * numberOfTokens});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'Cannot purchase more tokens than available');
         });
     });
 });
